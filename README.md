4e4ae6b8ec09528048acd0d10adddc1a5685b175
Stuart Runyan <shrunyan@gmail.com>
2019-09-02 18:15:03 -0700

    WIP: pretty close to complete. Just need to add tests to ensure all edge cases are meet.

--- README.md
+++ README.md
@@ -20,3 +20,12 @@ npm run test
 ```
 
 For detailed explanation on how things work, checkout the [CLI Readme](https://github.com/developit/preact-cli/blob/master/README.md).
+
+
+## Game Engine
+
+Starting into building the engine which will calculate combo matches and scores. Honestly not sure where to start. Read up on pattern matching a little. It seems to me that is what I'm trying to do. e.g. A user selcts 3 one's (1-1-1) and 2 fives (5-5). This would be a score of 1000 + 50 + 50 = 1500. Is that a pattern? 1-1-1-5-5? Does seem quite right. The only thing I know for sure is that I can't/don't want to `if` check for every single combo variation possibility.
+
+Realizing there are a few set of common combos I can check for. E.g. 3 of a kind, 4 of a kind, etc... Initial tests seem correct but there are a few edge cases which don't fit this thought process. e.g. Flush 1-2-3-4-5-6. Maybe unique `if` checks on just the edge cases. 
+
+Another problem I know I'll face and don't have an answer for is; how to determine which combo to select. e.g. In combo of 5-5-5-5-4-4. Is it best to go with 5-5-5-5 (1,000 points), 5-5 5-5 4-4 (1,500). I guess I just need to calculate and track every possible scoring combo and then compair results to determine the highest score. Because in the described scenario obviously the players intent is to score a triple pair (1,500). If I choose the 4 of a kind this would upset a player.
--- src/components/dice-board/index.js
+++ src/components/dice-board/index.js
@@ -19,9 +19,14 @@ export default class DiceBoard extends Component {
     });
 
     // TODO calculate score
-    // const score =
+    const score = this.engine(rollResult);
 
-    this.engine(rollResult);
+    console.log("SCORE", score);
+
+    // TODO farkle
+    if (score === 0) {
+      console.log("FARKLE");
+    }
 
     this.setState({
       dice: rollResult
@@ -30,11 +35,14 @@ export default class DiceBoard extends Component {
 
   engine = selections => {
     // console.log("engine", selections);
+    let score = 0;
 
-    // Sort default is alphabetically
+    // Sort default is alphabetical
     // We provide custom sort function to sort numerically
-    const numericalSort = selections.sort((a, b) => a - b);
-    const matches = numericalSort.reduce((acc, val) => {
+    const numericallySorted = selections.sort((a, b) => a - b);
+
+    // Find all die value matches
+    const matches = numericallySorted.reduce((acc, val) => {
       if (acc[val]) {
         acc[val].count = acc[val].count + 1;
       } else {
@@ -45,70 +53,133 @@ export default class DiceBoard extends Component {
       return acc;
     }, {});
 
-    console.log("matches", matches);
+    const matchedDice = Object.keys(matches);
 
-    const score = Object.keys(matches).reduce((acc, die) => {
+    console.log("matches", matches);
+    console.log("matched key count", matchedDice.length);
+
+    // Check for flush
+    if (numericallySorted.join("-") === "1-2-3-4-5-6") {
+      console.log("flush: 1-2-3-4-5-6");
+      score = 3000;
+      return score;
+    }
+
+    // 2 sets of 3 of a kind
+    // If we were given a full set of dice and there are only 2 match groups
+    if (numericallySorted.length === 6 && matchedDice.length === 2) {
+      // and each group contains a count of 3 then we have 2 sets of 3 of a kind
+      let pair = matchedDice.filter(die => matches[die].count === 3);
+      if (pair.length === 2) {
+        console.log("2 sets of 3 of a kind");
+        score = 2500;
+        return score;
+      }
+    }
+
+    // 3 sets of 2 of a kind
+    // a.k.a triple pair
+    if (numericallySorted.length === 6 && matchedDice.length === 3) {
+      let triple = matchedDice.filter(die => matches[die].count === 2);
+      if (triple.length === 3) {
+        console.log("triple pair");
+        score = 1500;
+        return score;
+      }
+    }
+
+    // All other cases
+    score = matchedDice.reduce((acc, die, index) => {
       let count = matches[die].count;
 
-      console.log("die", die);
-      console.log("count", count);
-
-      switch (count) {
-        case 6:
-          // six of a kind
+      switch (String(count)) {
+        case "6":
+          console.log("six of a kind");
           acc += 3000;
-        case 5:
-          // five of a kind
+          break;
+
+        case "5":
+          console.log("five of a kind");
           acc += 2000;
-        case 4:
-          // four of a kind
+          break;
+
+        case "4":
+          console.log("four of a kind");
           acc += 1000;
-        case 3:
-          // three of a kind
+          break;
+
+        // In the case of a triple pair. e.g. 5-5-5-5-4-4
+        // Which has a higher score is captured by the edge case
+        // checks before this main switch statement
+
+        case "3":
+          console.log("three of a kind");
           switch (die) {
-            case 6:
+            case "6":
               acc += 600;
-            case 5:
+              break;
+            case "5":
               acc += 500;
-            case 4:
+              break;
+            case "4":
               acc += 400;
-            case 3:
+              break;
+            case "3":
               acc += 300;
-            case 2:
+              break;
+            case "2":
               acc += 200;
-            case 1:
+              break;
+            case "1":
               acc += 1000;
+              break;
           }
-        case 2:
-          console.log("pair");
+          break;
 
-        // pair
-        // 1500 points
+        case "2":
+          console.log("1 & 5 pairs");
 
-        // TODO count pairs
-        // TODO count 4 of kind and pair
+          // NOTE: triple pairs are handled by the preceding edge case check
 
-        case 1:
-          if (die === 5) {
-            acc += 50;
-          }
-          if (die === 1) {
+          // handle 1-1 & 5-5 cases where there isn't a triple pair
+          // but these would score as 200 & 100 respectively
+
+          if (die == 5) {
             acc += 100;
           }
+          if (die == 1) {
+            acc += 200;
+          }
+          break;
+
+        case "1":
+          console.log("1 or 5 die");
+          if (die == 5) {
+            acc += 50;
+          }
+          if (die == 1) {
+            acc += 100;
+          }
+          break;
 
         default:
-        // no score
-        // 0 points
+          // no score
+          // 0 points
+          break;
       }
+
+      console.table({
+        index,
+        die,
+        count,
+        score: acc
+      });
 
       // Building up score
       return acc;
     }, 0);
 
-    // TODO figure out 2 sets of 3 of a kind
-    // TODO figure out flush: 1 2 3 4 5 6
-
-    console.log("score", score);
+    return score;
   };
 
   render(props, state) {
