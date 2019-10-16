18c3d4eaa50e998e1a33562b12bd60d3e1eefe1c
Stuart Runyan <shrunyan@gmail.com>
2019-10-15 19:36:53 -0700

    modified app structure to support hosting in Zesty

--- src/index.js
+++ src/index.js
@@ -2,40 +2,18 @@
 // require("preact/debug");
 
 import { h, Component } from "preact";
-import { Router } from "preact-router";
 import { Provider } from "unistore/preact";
 
 import store from "./store";
 
-// Code-splitting is automated for routes
-import Home from "./routes/home";
-import Play from "./routes/play";
-import Rules from "./routes/rules";
-import About from "./routes/about";
+import Play from "./views/play";
 
-import Header from "./components/header";
-
-import style from "./style";
 export default class App extends Component {
-  /** Gets fired when the route changes.
-   *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
-   *	@param {string} event.url	The newly routed URL
-   */
-  handleRoute = evt => {
-    this.currentUrl = evt.url;
-  };
-
   render() {
     return (
       <Provider store={store}>
-        <div id="app">
-          <Header />
-          <Router onChange={this.handleRoute}>
-            <Home path="/" />
-            <Play path="/play" />
-            <Rules path="/rules" />
-            <About path="/about" />
-          </Router>
+        <div id="game">
+          <Play />
         </div>
       </Provider>
     );
