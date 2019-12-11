2b254345b6683fa126bad7fbe289e4026aa9fe6b
Stuart Runyan <shrunyan@gmail.com>
2019-12-10 20:48:17 -0800

    switching to tailwindui preact plugin

--- preact.config.js
+++ preact.config.js
@@ -1,32 +1,18 @@
-const preactCliPostCSS = require("preact-cli-postcss");
-const PurgecssPlugin = require("purgecss-webpack-plugin");
-const glob = require("glob");
-// const path = require("path");
+// const preactCliPostCSS = require("preact-cli-postcss");
 
-// Custom PurgeCSS extractor for special characters in Tailwind's classnames
-class TailwindExtractor {
-  static extract(content) {
-    return content.match(/[A-z0-9-:\/]+/g) || [];
-  }
-}
-
-// PurgeCSS webpack plugin
-const purgeCssPlugin = new PurgecssPlugin({
-  paths: glob.sync("./components/**/*.js"),
-  extractors: [
-    {
-      extractor: TailwindExtractor,
-      extensions: ["js", "jsx"]
-    }
-  ]
-});
+const tailwind = require("preact-cli-tailwind");
+const tailwindPlugin = (config, env, helpers) => {
+  config = tailwind(config, env, helpers);
+  return config;
+};
 
 export default function(config, env, helpers) {
   // Use postcss.config.js instead of default postCSS config
-  preactCliPostCSS(config, helpers);
+  // preactCliPostCSS(config, helpers);
+  tailwindPlugin(config, env, helpers);
 
   // Run styles through purgeCSS for production only
-  if (env.production) {
-    config.plugins.push(purgeCssPlugin);
-  }
+  // if (env.production) {
+  //   // config.plugins.push(purgeCssPlugin);
+  // }
 }
